e6b303013ae4dd44bb10e93e73e2e93a7d1f7b2e
abdo <abdo@gmail.com>
2020-05-20 13:23:27 +0100

    Personal portfolio

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 class blog(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
     title_url = models.URLField(blank=True)
-    date = models.DateField
-    description = models.CharField(max_length=200)
+    date = models.DateField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import blog
 
 def all_blogs(request):
-    blogs = blog.objects.all()
+    blogs = blog.objects.order_by('-date')
     return render(request,"blog/all_blogs.html", {'blogs': blogs})
+
+def detail(request,blog_id):
+    blog_item = get_object_or_404(blog, pk=blog_id)
+    return render(request,'blog/detail.html', {'blog':blog_item})
